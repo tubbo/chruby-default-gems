fb02b897e8b3bcbe23528e8094dae21b0af345de
Scott Bronson <brons_github@rinspin.com>
2014-10-25 14:58:24 -0700

    install the command and update caveats to match

--- homebrew/chruby-default-gems.rb
+++ homebrew/chruby-default-gems.rb
@@ -4,9 +4,13 @@ class ChrubyDefaultGems < Formula
   homepage 'https://github.com/tubbo/chruby-default-gems#readme'
   url 'https://github.com/tubbo/chruby-default-gems/archive/master.zip'
   sha1 '9abfb6f906825229b7519f3a99fa749e6939f660'
-  version '0.0.1'
+  version '0.0.1' # delete this line once Homebrew can detect the version from t
 
   head 'https://github.com/tubbo/chruby-default-gems.git'
+
+  def install
+    FileUtils.cp "lib/chruby/default_gems.sh", "#{prefix}/default_gems.sh"
+  end
 
   def caveats
     %(
@@ -14,7 +18,7 @@ class ChrubyDefaultGems < Formula
 
       Copy the following line into your .bash_profile or .zshenv:
 
-        source #{prefix}/lib/chruby-default-gems/lib/chruby/default_gems.sh
+        source #{prefix}/default_gems.sh
 
       You can also set the DEFAULT_GEMFILE variable if you wish to store
       your default-gems in a location other than
