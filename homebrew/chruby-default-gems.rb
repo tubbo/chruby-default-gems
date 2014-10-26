71ffdae83b7a2e1f3f91bd156849b7c980be8c23
Scott Bronson <brons_git@rinspin.com>
2014-10-26 00:03:43 -0700

    update homebrew script with latest master

--- homebrew/chruby-default-gems.rb
+++ homebrew/chruby-default-gems.rb
@@ -1,12 +1,12 @@
 require 'formula'
 
 class ChrubyDefaultGems < Formula
-  homepage 'https://github.com/tubbo/chruby-default-gems#readme'
-  url 'https://github.com/tubbo/chruby-default-gems/archive/master.zip'
+  homepage 'https://github.com/bronson/chruby-default-gems#readme'
+  url 'https://github.com/bronson/chruby-default-gems/archive/master.zip'
   sha1 '9abfb6f906825229b7519f3a99fa749e6939f660'
   version '0.0.1' # delete this line once Homebrew can detect the version from t
 
-  head 'https://github.com/tubbo/chruby-default-gems.git'
+  head 'https://github.com/bronson/chruby-default-gems.git'
 
   def install
     FileUtils.cp "default-gems.sh", "#{prefix}/default-gems.sh"
